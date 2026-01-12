365976984406e154f5720033e4b42e2244d632a4
kiarapedes_reues <diceroam@gmail.com>
2026-01-12 21:14:25 +0700

    benerin denah

--- js/denah.js
+++ js/denah.js
@@ -3,26 +3,56 @@ var map = L.map("map-container", {
     minZoom: -1
 });
 
-var bounds = [[0,0], [1000,1000]];
-var image = L.imageOverlay("../gambar/denah_source_placeholder.png", bounds).addTo(map);
+var bounds = [[0,0], [2500, 2500]];
+var image = L.imageOverlay("../gambar/map.jpg", bounds).addTo(map);
 map.fitBounds(bounds);
 
 var lokasi = [
     {
         nama: "Kantor Desa Kiarapedes",
-        koordinat: [200, 300],
+        koordinat: [880, 1090],
         foto: "../gambar/kantor_desa.jpg",
         deskripsi: "Kantor Desa Kiarapedes adalah pusat administrasi dan pelayanan masyarakat desa."
     },
     {
         nama: "Lapangan Panca Waluya",
-        koordinat: [500, 600],
+        koordinat: [980, 270],
         foto: "../gambar/lapangan.jpeg",
         deskripsi: "Lapangan Panca Waluya adalah tempat untuk berbagai kegiatan aktivitas masyarakat."
     },
     {
         nama: "SMK Negeri 1 Kiarapedes",
-        koordinat: [700, 800],
+        koordinat: [1066, 666],
+        foto: "../gambar/smk.jpg",
+        deskripsi: "SMK Negeri 1 Kiarapedes adalah sekolah menengah kejuruan yang menyelenggarakan pendidikan dan pelatihan di bidang teknologi informasi dan komunikasi."
+    },
+    {
+        nama: "Sate Maranggi Pareang",
+        koordinat: [1132, 485],
+        foto: "../gambar/smk.jpg",
+        deskripsi: "SMK Negeri 1 Kiarapedes adalah sekolah menengah kejuruan yang menyelenggarakan pendidikan dan pelatihan di bidang teknologi informasi dan komunikasi."
+    },
+    {
+        nama: "Kantor Kecamatan Kiarapedes",
+        koordinat: [212, 505],
+        foto: "../gambar/kantor_kecamatan.jpg",
+        deskripsi: "Kantor Kecamatan Kiarapedes adalah pusat administrasi dan pelayanan masyarakat di tingkat kecamatan."
+    },
+    {
+        nama: "KUA Kecamatan Kiarapedes",
+        koordinat: [1006, 820],
+        foto: "../gambar/smk.jpg",
+        deskripsi: "SMK Negeri 1 Kiarapedes adalah sekolah menengah kejuruan yang menyelenggarakan pendidikan dan pelatihan di bidang teknologi informasi dan komunikasi."
+    },
+    {
+        nama: "Masjid Jami Al Baroqah",
+        koordinat: [691, 807],
+        foto: "../gambar/smk.jpg",
+        deskripsi: "SMK Negeri 1 Kiarapedes adalah sekolah menengah kejuruan yang menyelenggarakan pendidikan dan pelatihan di bidang teknologi informasi dan komunikasi."
+    },
+    {
+        nama: "SDN 1 Kiarapedes",
+        koordinat: [964, 1127],
         foto: "../gambar/smk.jpg",
         deskripsi: "SMK Negeri 1 Kiarapedes adalah sekolah menengah kejuruan yang menyelenggarakan pendidikan dan pelatihan di bidang teknologi informasi dan komunikasi."
     }
